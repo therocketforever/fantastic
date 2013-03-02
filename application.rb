669108464810cc41d1326ed887d36d6ce2252af5
DON who is a ghost <therocketforever@me.com>
2013-03-01 23:06:58 -0500

    Basic Application Logic

---
+++ application.rb
@@ -0,0 +1,27 @@
+class Application < Sinatra::Base
+  
+  enable :logging, :inline_templates
+
+  configure :test do
+    DataMapper.setup(:default, "sqlite://#{Dir.pwd}/features/support/test.db")
+  end
+ 
+  configure :development do
+    Bundler.require(:development)
+    DataMapper.setup(:default, "sqlite://#{Dir.pwd}/development.db")
+  end
+
+  configure :production do
+    DataMapper.setup(:default, ENV['DATABASE_URL']) 
+  end
+  
+  get '/' do
+    "Hello World!"
+  end
+end
+
+class Item
+  include DataMapper::Resource
+  
+  property :id, Serial
+end
